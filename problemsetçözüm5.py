cb319ca484f39d06ad2d7d27dacdc357a3e80b19
hrr11 <77586352+hrr11@users.noreply.github.com>
2021-02-02 16:28:14 +0300

    Update problemsetçözüm5.py

--- problemsetçözüm5.py
+++ problemsetçözüm5.py
@@ -1,1 +1,15 @@
+class Okul():
+    tip = "egitimhane"
 
+    def __init__(self, calisan=3, bina=1, idaribina=1, önbahce=True):
+        self.calisan = calisan
+        self.bina = bina
+        self.idaribina = idaribina
+        self.önbahce = önbahce
+
+
+okul1 = Okul(50, 1, 1, True)
+okul2 = Okul(20, 1, 2, False)
+okul3 = Okul(calisan=12, idaribina=1)
+print(okul3.calisan)
+print(okul1.__class__.tip)
